040ddc6356899466463aa2e460aff8dd2f0c3f86
wangchao <1023649157@qq.com>
2019-09-18 19:51:50 +0800

    web pages: add mobile support

--- docs/archive.js
+++ docs/archive.js
@@ -7,10 +7,11 @@ $(document).ready(function(){
 	});
 });
 
-var str =['(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop',
+var str1 =['(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop',
 		'|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone',
-		'|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda',
-		'|xiino|1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)',
+		'|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino'].join('')
+
+var str2 =['1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)',
 		'|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w',
 		'|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica',
 		'|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene',
@@ -25,12 +26,14 @@ var str =['(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal
 		'|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9',
 		'|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)',
 		'|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-'].join('')
-var myRE = new RegExp (str,'i');
+var myRE1 = new RegExp (str1,'i');
+var myRE2 = new RegExp (str2,'i');
 
 // $(document).ready(function(){alert(str)});
 $(document).ready(function(){alert((navigator.userAgent||navigator.vendor||window.opera).substr(0,4))});
 $(document).ready(function(){
-	if(myRE.test((navigator.userAgent||navigator.vendor||window.opera).substr(0,4))){
+	if(myRE1.test(navigator.userAgent||navigator.vendor||window.opera)||
+		myRE2.test((navigator.userAgent||navigator.vendor||window.opera).substr(0,4))){
 		alert("mobile");
 	}else{
 		alert("desktop");
